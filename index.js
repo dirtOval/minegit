b908526b2bc65d398faf8420c915f321142d91bc
deirdre <deirdre.loyall@gmail.com>
2023-08-26 19:35:49 -0400

    added echotest and pathtest routes to test CC http api and modifying CC files directly using fs module

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 const express = require('express');
-const app = express();
-const port = 42069;
+const bodyParser = require('body-parser');
+require('dotenv').config();
+var fs = require("fs");
 
+const app = express();
+const port = process.env.PORT;
+const rootDir = process.env.ROOTDIR;
+
+//middleware zone
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.json());
+// app.use(bodyParser.raw());
+
+//routes
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`Root dir is: ${rootDir}`);
+})
+
+app.get('/echotest', (req, res) => {
+  let echo = req.query.echo;
+  console.log(`CC says: ${echo}`);
+  res.send(`it did the thing`);
+})
+
+app.get('/pathtest', (req, res) => {
+  // console.log(req);
+  let ccPath = req.query.path;
+  fs.writeFile(`${process.env.ROOTDIR}/${ccPath}/banana.txt`, 'its a banana', err => {
+    if (err) throw err;
+    console.log(`adding banana to path "${ccPath}"`)
+    res.send("banana added :)");
+  })
 })
 
 app.listen(port, () => {
-  console.log(`Listening on port ${port}! Happy mining! :)`)
+  console.log(`Listening on port ${port}! Happy mining! :)`);
 })
